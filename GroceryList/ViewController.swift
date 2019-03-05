8704344c65644a701f39fb9de748a42b5e23fe63
ANGEL GASCA-CARDONA <rl230154@rlas-macair-36.local>
2019-03-05 11:32:50 -0600

    Initial Commit

---
+++ GroceryList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroceryList
+//
+//  Created by ANGEL GASCA-CARDONA on 3/5/19.
+//  Copyright © 2019 ANGEL GASCA-CARDONA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
